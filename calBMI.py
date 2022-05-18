133979d4b1d0ba2941f6b40d4aceb24fb5b15342
NCSU - Aaron <abpascua@ncsu.edu>
2022-05-17 22:54:25 -0400

    Can't fail on 0 input

--- calBMI.py
+++ calBMI.py
@@ -10,26 +10,38 @@
 import math
 import sys
 
-#Variables
+def getUserInput():
+    #Variables
+    userWeight = input("Enter your weight in pounds (lbs): ")
+    userHeight = input("Enter your height in feet (ft): ")
+    calcBMI(userWeight, userHeight)
+
+def calcBMI(userWeight, userHeight):
+    #Check to see if input is numerical
+    if not str(userWeight).isnumeric() or not str(userHeight).isnumeric():
+        print("Invalid entry")
+        sys.exit(-1)
+    if int(userWeight) == 0 or int(userHeight) == 0:
+        print ("Invalid weight or height")
+        getUserInput()
+    else:
+        #Convert user inputs to floats
+        weightFloat = float(userWeight)
+        heightFloat = float(userHeight)
+        
+        #Imperial to metric conversions
+        lbs2kg = weightFloat * 0.453592
+        ft2m = heightFloat * 0.3048
+        
+        #Calculate BMI
+        BMI = lbs2kg / (math.pow(ft2m,2))
+        
+        #Print weight, height, and BMI
+        print('Weight = {0} lbs = {1} Kg,'.format(format(weightFloat,".3f"), format(lbs2kg,".3f")))
+        print('Height = {0} feet = {1} m,'.format(format(heightFloat,".3f"), format(ft2m,".3f"))) 
+        print('BMI = {0}'.format(format(BMI,".3f")))
+        
+
 userWeight = input("Enter your weight in pounds (lbs): ")
 userHeight = input("Enter your height in feet (ft): ")
-
-#Check to see if input is numerical
-if not str(userWeight).isnumeric() or not str(userHeight).isnumeric():
-    sys.exit(-1)
-else:
-    #Convert user inputs to floats
-    weightFloat = float(userWeight)
-    heightFloat = float(userHeight)
-    
-    #Imperial to metric conversions
-    lbs2kg = weightFloat * 0.453592
-    ft2m = heightFloat * 0.3048
-    
-    #Calculate BMI
-    BMI = lbs2kg / (math.pow(ft2m,2))
-    
-    #Print weight, height, and BMI
-    print('Weight = {0} lbs = {1} Kg,'.format(format(weightFloat,".3f"), format(lbs2kg,".3f")))
-    print('Height = {0} feet = {1} m,'.format(format(heightFloat,".3f"), format(ft2m,".3f"))) 
-    print('BMI = {0}'.format(format(BMI,".3f")))
+calcBMI(userWeight, userHeight)
