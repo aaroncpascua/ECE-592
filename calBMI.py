2a0a24c1c1cdd84bd946668e48e0760b0e67d8fa
NCSU - Aaron <abpascua@ncsu.edu>
2022-05-17 22:37:31 -0400

    Changed values to 3 decimal points; exits if input is non-numerical

--- calBMI.py
+++ calBMI.py
@@ -8,19 +8,28 @@
 #BMI = ____
 
 import math
+import sys
 
 #Variables
-userWeight = float(input("Enter your weight in pounds (lbs): "))
-userHeight = float(input("Enter your height in feet (ft): "))
+userWeight = input("Enter your weight in pounds (lbs): ")
+userHeight = input("Enter your height in feet (ft): ")
 
-#Imperial to metric conversions
-lbs2kg = userWeight * 0.453592
-ft2m = userHeight * 0.3048
-
-#Calculate BMI
-BMI = lbs2kg / (math.pow(ft2m,2))
-
-#Print weight, height, and BMI
-print('Weight = {0} lbs = {1} Kg,'.format(userWeight, lbs2kg))
-print('Height = {0} feet = {1} m,'.format(userHeight, ft2m)) 
-print('BMI = {0}'.format(BMI))
+#Check to see if input is numerical
+if not str(userWeight).isnumeric() or not str(userHeight).isnumeric():
+    sys.exit(-1)
+else:
+    #Convert user inputs to floats
+    weightFloat = float(userWeight)
+    heightFloat = float(userHeight)
+    
+    #Imperial to metric conversions
+    lbs2kg = weightFloat * 0.453592
+    ft2m = heightFloat * 0.3048
+    
+    #Calculate BMI
+    BMI = lbs2kg / (math.pow(ft2m,2))
+    
+    #Print weight, height, and BMI
+    print('Weight = {0} lbs = {1} Kg,'.format(format(weightFloat,".3f"), format(lbs2kg,".3f")))
+    print('Height = {0} feet = {1} m,'.format(format(heightFloat,".3f"), format(ft2m,".3f"))) 
+    print('BMI = {0}'.format(format(BMI,".3f")))
