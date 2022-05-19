1c53e48fb70fd6e8f355bed890275da74c52a714
NCSU - Aaron <abpascua@ncsu.edu>
2022-05-18 21:16:09 -0400

    Added findSquares and calSalary - maybe needs to be tuned depending on professor response

--- Homework1.py
+++ Homework1.py
@@ -1,4 +1,4 @@
-pyramidInput = input("Enter string for message pyramid: ")
+import math
 
 def pyramid(s):
     '''Takes a user input and creates a pyramid pattern of the letters
@@ -9,4 +9,51 @@ def pyramid(s):
         strList.append(s[:i])
     print("\n".join(strList))
     
-pyramid(pyramidInput)
+def findSquares(s:int, e:int):
+    '''Find and print a list of numbers which are exact squares of an 
+    integer between two user inputted integer values'''
+    
+    #Find the lowest number of the two integers
+    if s < e:
+        lowNum = s
+        highNum = e
+    elif s == 0:
+        lowNum = 0
+        highNum = e
+    elif e == 0:
+        lowNum = 0
+        highNum = s
+    else:
+        lowNum = e
+        highNum = s
+    
+    #Loop through the numbers, find perfect squares, and store into list
+    perfRoots = []
+    for x in range(lowNum, highNum):
+        root = math.sqrt(x)
+        if int(root + 0.5) ** 2 == x:
+            #print(x)
+            perfRoots.append(x)
+    print(perfRoots)
+    
+def calSalary(h:float, r:float):
+    '''Calculate salary given number of hours and hourly rate. Calculates
+    overtime rate if hours are greater than 40.'''
+    
+    if h < 0:
+        print("Not valid hours")
+        return -1
+    elif h > 40:
+        rate = r + (r * 0.2)
+        hours = (h - 40)
+        salary = (40 * r) + (hours * rate)
+    else:
+        salary = h * r
+    return salary
+    
+#pyramidInput = input("Enter string for message pyramid: ")
+#pyramid(pyramidInput)
+
+#findSquaresInput1 = int(input("Enter first number: "))
+#findSquaresInput2 = int(input("Enter second numeber: "))
+#findSquares(findSquaresInput1, findSquaresInput2)
