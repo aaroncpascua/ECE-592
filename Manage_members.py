eacb0dd97cb2bebef07a28b94338f97c61689eca
NCSU - Aaron <abpascua@ncsu.edu>
2022-06-16 01:00:05 -0400

    added readMemberCSV

--- Manage_members.py
+++ Manage_members.py
@@ -1,6 +1,9 @@
-from gen_member_data import *
+import csv
+
+memberDict = {}
 
 def Manage_members():
+    memberDict = readMemberCSV()
     print('Menu:')
     print('a. Add a new member')
     print('b. Remove member')
@@ -13,7 +16,7 @@ def Manage_members():
     userInput = input('Please choose an option: ')
     
     if userInput == 'a':
-        addNewMember()
+        addNewMember(memberDict)
     elif userInput == 'b':
         return
     elif userInput == 'c':
@@ -33,13 +36,31 @@ def Manage_members():
         Manage_members()
      
 # %% Add New Member functions
-def addNewMember():
-    firstName = inputFirstName()
-    middleInitial = inputMiddleIniital()
-    lastName = inputLastName()
-    dateOfBirth = inputBirthday()
-    address = inputAddress()
-    status = inputStatus()
-    
+def addNewMember(tempDict):
+    print("\n\n\n\n\nAdding new member:")
+    inputFirstName = input("Enter first name: ")
+    inputLastName = input("Enter last name: ")
+    inputDateOfBirth = input("Enter Date of Birth (Example: June 15 1996): ")
+    inputAddress = input("Enter address: ")
+    inputStatus = input("What member status do you want? (Basic, Silver, Gold, Platinum)")
 
-Manage_members()
+def readMemberCSV():
+    tempDict = {}
+    with open('memberdata.csv', mode='r') as inFile:
+        keyReader = csv.reader(inFile)
+        key = []
+        key = next(keyReader)
+        for i in range(0, len(key)):
+            tempDict[key[i]] = []
+        inFile.close()
+        
+    with open('memberdata.csv', mode='r') as inFile:    
+        recordReader = csv.DictReader(inFile)
+        for record in recordReader:
+            for k in tempDict:
+                tempDict[k].append(record[k])
+    
+    return tempDict
+
+#Manage_members()
+readMemberCSV()
